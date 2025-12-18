2b5286416ba6be8b891a864a8f2589e9af4b0d6c
Yatendra Kumar <yatendrakumar18113@gmail.com>
2025-12-18 11:06:52 +0530

    Implemented support feature

--- frontend/src/landing_page/support/CreateTicket.jsx
+++ frontend/src/landing_page/support/CreateTicket.jsx
@@ -1,8 +1,106 @@
-import React from 'react';
+import React from "react";
+
 function CreateTicket() {
-    return ( 
-        <h1>Create Ticket</h1>
-     );
+    const ticketData = [
+  {
+    title: "Account Opening",
+    links: [
+      "Online account opening",
+      "Offline account opening",
+      "Company, partnership and HUF account opening",
+      "NRI account opening",
+      "Charges at Zerodha",
+      "Zerodha IDFC FIRST Bank 3-in-1 account",
+      "Getting started",
+    ],
+  },
+  {
+    title: "Your Zerodha Account",
+    links: [
+      "Login credentials",
+      "Account modification and segment addition",
+      "DP ID and bank details",
+      "Your Profile",
+      "Account closure",
+    ],
+  },
+  {
+    title: "Trading and Markets",
+    links: [
+      "Trading FAQs",
+      "Kite features",
+      "Margins",
+      "Product and order types",
+      "Corporate actions",
+      "Kite user manual",
+    ],
+  },
+  {
+    title: "Funds",
+    links: [
+      "Adding funds",
+      "Withdrawing funds",
+      "Funds transfer",
+      "Pledge and unpledge",
+      "Funds statement",
+    ],
+  },
+  {
+    title: "Console",
+    links: [
+      "Reports",
+      "Ledger",
+      "Portfolio",
+      "Tax reports",
+      "Corporate actions",
+    ],
+  },
+  {
+    title: "Coin",
+    links: [
+      "Understanding mutual funds",
+      "Coin app",
+      "Investing in mutual funds",
+      "Statements and reports",
+      "FAQs",
+    ],
+  },
+];
+
+  return (
+    <div className="container mt-5 mb-5">
+  <h1 className="fs-2 mb-4">
+    To create a ticket, select a relevant topic
+  </h1>
+
+  <div className="row">
+    {ticketData.map((section, index) => (
+      <div key={index} className="col-md-4 px-4 py-3">
+        <h4 className="mb-3">
+          <i className="fa fa-plus-circle me-2" aria-hidden="true"></i>
+          {section.title}
+        </h4>
+
+        <div className='ps-4'>
+            {section.links.map((link, i) => (
+              <div key={i}>
+                <a
+                  href="/"
+                  onClick={(e) => e.preventDefault()}
+                  style={{ textDecoration: "none", lineHeight: "1.9" }}
+                >
+                  {link}
+                </a>
+              </div>
+            ))}
+        </div>
+      </div>
+    ))}
+  </div>
+</div>
+
+
+  );
 }
 
 export default CreateTicket;
--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React from "react";
+
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <section className="container-fluid" id="supportHero">
+      <div className="p-5 " id="supportWrapper">
+        <h4>Support Portal</h4>
+        <a href="">Track Tickets</a>
+      </div>
+      <div className="row  m-5">
+        <div className="col-1"></div>
+        <div className="col-5 p-5">
+          <h1 className="fs-3 mb-4">
+            Search for an answer or browse help topics to create a ticket
+          </h1>
+          <input placeholder="Eg. how do I activate F&O or raise a complaint" />
+          <br />
+          <div className='mt-4 d-flex gap-3 flex-wrap'>
+              <a href="">Track account opening</a>
+              <a href="">Track segment activation</a>
+              <a href="">Intraday margins</a>
+              <a href="">Kite user manual</a>
+          </div>
+        </div>
+        <div className="col-5 p-5 ">
+          <h1 className="fs-3">Featured</h1>
+          <ol className='p-3'>
+            <li className='mt-4 mb-2'>
+              <a href="">Current Takeovers and Delisting - January 2024</a>
+            </li>
+            <li>
+              <a href="">Latest Intraday leverages - MIS & CO</a>
+            </li>
+          </ol>
+        </div>
+        <div className="col-1"></div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
